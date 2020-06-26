cbf75bbaa771e3726d13599e9088649689547119
hazae41 <hazae41@gmail.com>
2020-06-27 00:17:16 +0200

    fix

--- wasm.ts
+++ wasm.ts
@@ -1,7 +1,16 @@
 export async function Wasm(path: string) {
   const url = new URL(`./pkg/${path}`, import.meta.url);
-  const code = await Deno.readFile(url);
+  const code = await read(url);
   const module = new WebAssembly.Module(code);
   const instance = new WebAssembly.Instance(module);
   return instance.exports;
+}
+
+async function read(url: URL) {
+  if (url.protocol === "file:") {
+    return await Deno.readFile(url);
+  }
+
+  const response = await fetch(url);
+  return await response.arrayBuffer();
 }
