a17f87cd10ba4ad0b7e67a022a6dae66ff14fe86
hazae41 <hazae41@gmail.com>
2020-06-29 23:25:33 +0200

    better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
 use wasm_bindgen::prelude::*;
 
-use flate2::read::DeflateDecoder;
+use flate2::write::DeflateDecoder;
 use flate2::write::DeflateEncoder;
-
 use flate2::Compression;
 
-use std::io::{Read, Write};
+use std::io::Write;
 
 #[wasm_bindgen]
-pub fn inflate(input: &[u8]) -> Vec<u8> {
-    let mut d = DeflateDecoder::new(&input[..]);
-    let mut buffer = Vec::new();
-    d.read_to_end(&mut buffer).unwrap();
-    return buffer;
+pub fn inflate(input: &[u8]) -> Result<Vec<u8>, JsValue> {
+    let mut decoder = DeflateDecoder::new(Vec::new());
+
+    decoder
+        .write_all(input)
+        .and_then(|_| decoder.finish())
+        .map_err(|e| JsValue::from(&format!("{:?}: {}", e.kind(), e)))
 }
 
 #[wasm_bindgen]
-pub fn deflate(input: &[u8]) -> Vec<u8> {
-    let mut e = DeflateEncoder::new(Vec::new(), Compression::default());
-    e.write_all(input).unwrap();
-    let compressed_bytes = e.finish();
-    return compressed_bytes.unwrap();
+pub fn deflate(input: &[u8]) -> Result<Vec<u8>, JsValue> {
+    let mut encoder = DeflateEncoder::new(Vec::new(), Compression::default());
+
+    encoder
+        .write_all(input)
+        .and_then(|_| encoder.finish())
+        .map_err(|e| JsValue::from(&format!("{:?}: {}", e.kind(), e)))
 }
