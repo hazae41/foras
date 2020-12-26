463d21e9371ab790962a7d367de622277b6d99bb
Haz Æ 41 <hazae41@gmail.com>
2020-12-26 11:45:14 +0100

    Rename initWasm to init and export initialized

--- mod.ts
+++ mod.ts
@@ -7,23 +7,22 @@ export {
   unzlib,
 } from "./pkg/denoflate.js";
 
-import init from "./pkg/denoflate.js";
+import init as wasm from "./pkg/denoflate.js";
+
+export let initialized = false;
 
 async function read(path: string) {
   const url = new URL(path, import.meta.url);
 
-  if (url.protocol === "file:") {
+  if (url.protocol === "file:")
     return await Deno.readFile(url);
-  }
 
   const res = await fetch(url)
   return await res.arrayBuffer();
 }
 
-let wasmInitialised;
-export async function initWasm() {
-  if (!wasmInitialised) {
-    await init(read("./pkg/denoflate_bg.wasm"));
-    wasmInitialised = true;
-  }
+export async function init() {
+  if (initialized) return;
+  await wasm(read("./pkg/denoflate_bg.wasm"));
+  initialised = true;
 }
